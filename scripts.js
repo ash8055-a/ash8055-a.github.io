2eac740c22a65904e3bf0c16fcf72c01da293e9f
ash8055 <ash8055.yt@gmail.com>
2025-09-08 17:01:46 -0400

    Make GitHub button work

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const goBtn = document.getElementById("goBtn");
-  const ghBtn = document.getElementById("ghBtn");
+  const ghBtn = document.getElementById("ghProfileBtn");
 
   if (goBtn) {
     goBtn.addEventListener("click", () => {
       window.location.href = "/login";
     });
   }
+
+if (ghProfileBtn) {
+    ghProfileBtn.addEventListener("click", () => {
+      window.location.href = "https://github.com/ash8055-a";
+    });
+  }
+});
