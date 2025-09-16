7b92a6e167f54575bc36696af2f9093e997b0535
ash8055 <ash8055.yt@gmail.com>
2025-09-16 19:20:09 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const ghBtn = document.getElementById("ghProfileBtn");
+  const projectsBtn = document.getElementById("projectsBtn");
   if (ghBtn) {
     ghBtn.addEventListener("click", () => {
       window.open("https://github.com/ash8055-a", "_blank");
+    });
+  }
+
+  if (projectsBtn) {
+    ghBtn.addEventListener("click", () => {
+      window.open("/projects", "_blank");
     });
   }
 });
