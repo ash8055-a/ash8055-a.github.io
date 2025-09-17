7ecc222900db93757bb457a59965d76f9f6fa38a
ash8055 <ash8055.yt@gmail.com>
2025-09-17 15:37:50 -0400

    Update scripts.js

--- projects/scripts.js
+++ projects/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const playdatebtn = document.getElementById("playdateBtn");
   if (playdatebtn) {
     playdatebtn.addEventListener("click", () => {
-      window.location.href = "/playdate";
+      window.location.href = "/projects/playdate";
     });
   }
 });
