2530d1c0cf9a43162327006e7da8e879d93481e7
ash8055 <ash8055.yt@gmail.com>
2025-11-12 12:53:06 -0500

    Add social button event listener

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const ghBtn = document.getElementById("ghProfileBtn");
   const projectsBtn = document.getElementById("projectsBtn");
+  const socialBtn = document.getElementById("socialBtn");
+  
   if (ghBtn) {
     ghBtn.addEventListener("click", () => {
       window.open("https://github.com/ash8055-a", "_blank");
@@ -10,6 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
   if (projectsBtn) {
     projectsBtn.addEventListener("click", () => {
       window.location.href = "/projects";
+    });
+  }
+  
+  if (socialBtn) {
+    socialBtn.addEventListener("click", () => {
+      window.location.href = "/social";
     });
   }
 });
