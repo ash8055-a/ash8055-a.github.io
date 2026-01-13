fe044e2fff77017d1ddc19f5aecbbf71e04d54fb
ash8055 <ash8055.yt@gmail.com>
2026-01-13 11:21:26 -0500

    Update playdate button redirect URL

--- projects/scripts.js
+++ projects/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const playdatebtn = document.getElementById("playdateBtn");
   if (playdatebtn) {
     playdatebtn.addEventListener("click", () => {
-      window.location.href = "/projects/playdate/home.html";
+      window.location.href = "/projects/playdate/index.html";
     });
   }
 });
